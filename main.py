607c5ff19b8ba19f688c7d74fc81e790e21491d7
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-05-07 08:13:32 +0300

    added basic git bullshit

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ from settings import LiveUrl, BaseUrl
 
 
 print(requests.get(BaseUrl, verify=False).content)
+
+
+
+def AddFilesToGit():
+    print("making the git commit")
+    GitCommitMessage = "Live now at" + LiveUrl
+    print(os.system("git add ."))
+    print(os.system("git commit -am "+chr(34)+GitCommitMessage+chr(34)))
+    print(os.system("git push"))
+    pass
