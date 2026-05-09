5cc43090e0ea95dd2a4aad8c5cc3ba9e689610a1
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-05-09 18:03:32 +0300

    cleaned up the code

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import requests, os, re
-from settings import LiveUrl, BaseUrl, Disclosure, LiveImageUrl
-from datetime import date, datetime
+from settings import LiveUrl, BaseUrl, LiveImageUrl
 
 def FetchXml(Url):
     return (requests.get(Url, verify=False).content).decode()
 
 def MakeXml(SourceXml, ImageUrl):
-    # print(re.sub("https...open.live.bbc.co.uk.*mp3", "test" ,SourceXml))
-    # print(re.sub("https...ichef.bbci.co.uk.*jpg", "test" ,SourceXml))
-
-
     XmlNewImage = re.sub("https...ichef.*jpg", LiveImageUrl , SourceXml)
     XmlNewTitle = re.sub("BBC News","BBC News (Mirror)", XmlNewImage)
     XmlPodTitle = re.sub("T<.title>","T (Mirror)</title>", XmlNewTitle)
     
-
     FinalXml = XmlPodTitle
     return FinalXml
 
@@ -22,8 +16,6 @@ def WriteToDisk(Xml):
     FilePath = str(os.getcwd()) + "/index.html"
     with open(FilePath, "w",) as XmlFile:
         XmlFile.write(Xml)
-
-
 
 def AddFilesToGit():
     print("making the git commit")
